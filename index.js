06f599399fa9ad5c909839895024e1343aa9efaf
maiorem <61309793+maiorem@users.noreply.github.com>
2020-04-08 20:00:16 +0900

    css 수정 필요

---
+++ index.js
@@ -0,0 +1,12 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked"
+
+function handlerClick(){
+
+}
+
+function init() {
+  title.addEventListener("click", handlerClick);
+}
+init();
